0bba1c128ecc915e112f3c8606fe83aeadae20e8
Hudson Oliveira <hudson.o.pereira@gmail.com>
2023-12-11 22:22:15 -0300

    first step

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,7 @@
+<h1>Contato</h1>
+
+@if($nome == "Hudson")
+<p>{{$nome}}</p>
+@else
+<p>Outro nome</p>
+@endif
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+
     return view('welcome');
+});
+
+Route::get('/contact', function () {
+
+    $nome = "Ramona";
+
+    return view('contact', ['nome' => $nome]);
 });
