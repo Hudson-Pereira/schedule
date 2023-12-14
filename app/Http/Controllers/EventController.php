706a1e2a634a0a1b7be4f4ead66f1ead0e26dfe0
Hudson Oliveira <hudson.o.pereira@gmail.com>
2023-12-13 21:25:22 -0300

    feat: teste css welcome view, create table and model event

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Event; //importando model event (model acessa o banco)
 use Illuminate\Http\Request;
 
 class EventController extends Controller
@@ -9,7 +10,9 @@ class EventController extends Controller
 
     public function index()
     {
-        return view('welcome');
+        $events = Event::all(); //all() seleciona todos os dados da db
+
+        return view('welcome', ['events' => $events]); //enviar dados para a view entre []
     }
 
     public function create()
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,30 @@
 @extends('layouts.main')
 
-@section('title', 'Bem vindo')
+@section('title', 'Eventos')
 
 @section('content')
 
-<h1>Bem vindo!</h1>
+<h1>Eventos agendados</h1>
+
+<div id="search-container" class="col-md-12">
+    <h1>Busque um evento</h1>
+    <form action="">
+        <input type="text" id="serach" class="form-control" placeholder="Procurar...">
+    </form>
+</div>
+
+<div id="events-container" class="col-md-12">
+    <h2>Pŕoximos eventos</h2>
+    <div id="cards-container" class="row">
+        @foreach($events as $event)
+        <div class="card col-md-3">
+            <img src="../image/fundo.jpg" alt="{{$event->title}}">
+            <h5 class="card-title">{{$event->title}}</h5>
+            <p class="card-date">{{$event->date}}</p>
+            <a href="" class="btn btn-primary">Saber mais</a>
+        </div>
+        @endforeach
+    </div>
+</div>
 
 @endsection
